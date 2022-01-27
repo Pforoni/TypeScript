3f0ec706f42980eb455807938d0a0ea34a8ec045
pforoni <paulo.foroni@gmail.com>
2022-01-26 21:31:18 -0300

    Add novas functions

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,17 @@
 const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 const button = document.getElementById("button"); //corrije o erro do compilador achar q o objeto é null;
+const button2 = document.getElementById("button2"); //corrije o erro do compilador achar q o objeto é null;
 function sum(a, b) {
     return a + b;
 }
+let sub = (x, y) => {
+    return x - y;
+};
 button.addEventListener("click", function () {
     console.log(sum(Number(input1.value), Number(input2.value)));
     console.log("Paulo Augusto N.");
+});
+button2.addEventListener("click", function () {
+    console.log(sub(Number(input1.value), Number(input2.value)));
 });
--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 const input1 = document.getElementById("num1") as HTMLInputElement;
 const input2 = document.getElementById("num2") as HTMLInputElement;
 const button = document.getElementById("button")! //corrije o erro do compilador achar q o objeto é null;
+const button2 = document.getElementById("button2")! //corrije o erro do compilador achar q o objeto é null;
 
 function sum(a: number, b: number) {
     return a + b;
 }
 
+let sub = (x: number, y: number): number => {
+    return x - y;
+}
+
 button.addEventListener("click", function () {
-   console.log(sum(Number(input1.value), Number(input2.value)));
-   console.log("Paulo Augusto N.");
+    console.log(sum(Number(input1.value), Number(input2.value)));
+    console.log("Paulo Augusto N.");
+});
+
+button2.addEventListener("click", function () {
+    console.log(sub(Number(input1.value), Number(input2.value)));
 });
